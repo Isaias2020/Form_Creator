c6673b08acbbbfe8cbf37f9c0508f9c7056758b3
Isaias Quintanilla <quintanillaisaias@gmail.com>
2022-01-30 20:59:35 -0600

    Created another async function for submitted data.

--- index.js
+++ index.js
@@ -26,4 +26,45 @@ async function getData() {
     }
 }
 
+async function submit(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("fullname").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const occupation = document.getElementById("select-occupation");
+    const occupationOption = occupation.options[occupation.selectedIndex].text;
+    const state = document.getElementById("select-state");
+    const stateOption = state.options[state.selectedIndex].text;
+
+    form.onsubmit = submit;
+
+    const data = {
+        name: name,
+        email: email,
+        password: password,
+        occupation: occupationOption,
+        state: stateOption
+    }
+    console.log(JSON.stringify(data));
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    fetch(fetchRewards_url, options)
+        .then(res => {
+            console.log(res)
+            if (res.ok) {
+                document.getElementById('thanks').removeAttribute("hidden");
+                console.log(document.getElementById("thanks"));
+            }
+        })
+}
+
+const form = document.getElementById("form");
+form.addEventListener("submit", submit);
 getData();
